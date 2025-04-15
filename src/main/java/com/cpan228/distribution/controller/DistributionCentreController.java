a95eb4ba183c1e76ee42acf0fd0ff1090cffcde1
shelovesJ <jamesgriffiths23@hotmail.com>
2025-04-15 01:10:06 -0400

    Just imported libraries

--- src/main/java/com/cpan228/distribution/controller/DistributionCentreController.java
+++ src/main/java/com/cpan228/distribution/controller/DistributionCentreController.java
@@ -1,12 +1,15 @@
 package com.cpan228.distribution.controller;
 
-import com.cpan228.distribution.data.DistributionCentreRepository;
-import com.cpan228.distribution.model.DistributionCentres;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.cpan228.distribution.data.DistributionCentreRepository;
+import com.cpan228.distribution.model.DistributionCentres;
 
 @RestController
 @RequestMapping("/api/distribution-centres")
